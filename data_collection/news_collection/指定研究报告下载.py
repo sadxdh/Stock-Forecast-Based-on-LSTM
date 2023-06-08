1f0a856196615055c4a7b75e0aba7fce764178d4
sadxdh <2461366932@qq.com>
2023-06-08 23:39:05 +0800

    add research reports

--- data_collection/news_collection/指定研究报告下载.py
+++ data_collection/news_collection/指定研究报告下载.py
@@ -14,34 +14,71 @@ ua = fake_useragent.UserAgent()
 
 # 代理池
 def getproxy():
-    # proxypool_url = 'http://127.0.0.1:5555/random'
-    # proxy = requests.get(proxypool_url).text.strip()
-    # proxies = {'http': 'http://' + proxy}
-    # proxies = {'http': 'http://127.0.0.1:7890'}
-    # 连接到本地Redis数据库
-    redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
-    # 获取所有键名为"use_proxy"的键的值
-    all_values = redis_client.hvals('use_proxy')
-
-    # 从值中筛选出"last_status": true的键
-    filtered_keys = []
-    for value in all_values:
-        data = json.loads(value.decode('utf-8'))
-        if data.get('last_status') is True:
-            filtered_keys.append(data)
-
-    # 从筛选后的键中随机选择一个键
-    proxy = random.choice(filtered_keys)['proxy']
+    # proxypool_url = 'http://984094355034099712:AKUTpOB8@127.0.0.1:26763'
+    # proxy = requests.get(proxypool_url).text
     # print(proxy)
-    proxies = {'http': f'http://{proxy}', 'https': f'https://{proxy}'}
+    proxy = '984094355034099712:AKUTpOB8@127.0.0.1:26764'
+    # proxies = {'http': 'http://' + proxy}
+    import requests
+
+    target_url = "http://httpbin.org/ip"
+    proxy_host = 'http-dynamic.xiaoxiangdaili.com'
+    proxy_port = 10030
+    proxy_username = '984100790853849088'
+    proxy_pwd = 'Ajk8IMYg'
+
+    proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
+        "host": proxy_host,
+        "port": proxy_port,
+        "user": proxy_username,
+        "pass": proxy_pwd,
+    }
+
+    proxies = {
+        'http': proxyMeta,
+        'https': proxyMeta,
+    }
+
+    try:
+        resp = requests.get(url=target_url, proxies=proxies)
+        # print(resp.text)
+    except Exception as e:
+        print(e)
     return proxies
+
+    # # 连接到本地Redis数据库
+    # redis_client = redis.Redis(host='localhost', port=6379, db=0)
+    #
+    # # 提取key为'proxies:universal'的值
+    # value = redis_client.zrangebyscore('proxies:universal', min=10, max=float('inf'), withscores=True)
+    #
+    # # 从符合条件的值中随机抽取一个key
+    # random_key = random.choice(value)[0].decode('utf-8')
+    # print(random_key)
+    # proxy = random.choice(value).decode('utf-8')
+    # # print(proxy)
+    # proxies = {'http': f'http://{proxy}', 'https': f'https://{proxy}'}
+
+    # # 连接到本地Redis数据库
+    # redis_client = redis.Redis(host='localhost', port=6379, db=0)
+    # # 获取所有键名为"use_proxy"的键的值
+    # all_values = redis_client.hvals('use_proxy')
+    # # 从值中筛选出"last_status": true的键
+    # filtered_keys = []
+    # for value in all_values:
+    #     data = json.loads(value.decode('utf-8'))
+    #     if data.get('last_status') is True:
+    #         filtered_keys.append(data)
+    # # 从筛选后的键中随机选择一个键
+    # proxy = random.choice(filtered_keys)['proxy']
+    # # print(proxy)
+    # proxies = {'http': f'http://{proxy}', 'https': f'https://{proxy}'}
 
 
 def getNumPage(code):
     # https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=002415&t1=all
     url = f"https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol={code}&t1=all"
-    resp = requests.get(url, headers={'User-Agent': ua.ie}, proxies=getproxy(), verify=False)
+    resp = requests.get(url, headers={'User-Agent': ua.firefox}, proxies=getproxy(), verify=False)
     # print(resp.text)
     # page_count = 1
     page_count = etree.HTML(resp.text).xpath('//*[@id="_function_code_page"]/span[10]/a/@onclick')[0].split("'")[1]
@@ -53,28 +90,30 @@ def getNumPage(code):
 def reports(stock, code, page):
     global resp, htmlpage
     print(f"{stock}研究报告第{page}页")
-    # https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=002415&t1=all&p=16
+    # https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=600000&t1=all&p=8
     url = f"https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol={code}&t1=all&p={page}"
+    print(url)
     while True:
         try:
-            resp = requests.get(url, headers={'User-Agent': ua.ie}, proxies=getproxy(), verify=False)
+            resp = requests.get(url, headers={'User-Agent': ua.firefox}, proxies=getproxy(), verify=False)
         except ProxyError:
             print(f'{ProxyError},IP失效，标题页面内容无法获取，正在更换IP。。。。')
         else:
             # print(resp.text)
             page_html = etree.HTML(resp.text)
             trs = ['https:' + i for i in page_html.xpath('//td[@class="tal f14"]/a/@href')]
-            # print(trs)
+            print(trs)
             csv_file = open('data//news_data//' + f'{stock}研究报告第{page}页.csv', 'w', newline='', encoding='utf-8')
             f = csv.writer(csv_file)
             for tr in trs:
                 time.sleep(2)
                 while True:
                     try:
-                        htmlpage = requests.get(url, headers={'User-Agent': ua.ie}, proxies=getproxy(), verify=False)
+                        htmlpage = requests.get(tr, headers={'User-Agent': ua.firefox}, proxies=getproxy(), verify=False)
                     except ProxyError:
-                        print(f'{ProxyError},IP失效，标题页面内容无法获取，正在更换IP。。。。')
+                        print(f'{ProxyError},IP失效，研究报告页面内容无法获取，正在更换IP。。。。')
                     else:
+                        # print(htmlpage.text)
                         html = etree.HTML(htmlpage.text)
                         title = html.xpath('//div[@class="content"]/h1/text()')
                         category = html.xpath('//div[@class="content"]/div[1]/span[1]/text()')
@@ -82,9 +121,9 @@ def reports(stock, code, page):
                         researcher = html.xpath('//div[@class="content"]/div[1]/span[3]/a/text()')
                         date = html.xpath('//div[@class="content"]/div[1]/span[4]/text()')
                         content = html.xpath('//div[@class="content"]/div[2]/p/text()')
+                        print([title[0], category[0][3:], institution[0], researcher[0], date[0][3:], ''.join(content).strip()])
                         f.writerow([title[0], category[0][3:], institution[0], researcher[0], date[0][3:], ''.join(content).strip()])
-                        # print([title[0], category[0][3:], institution[0], researcher[0], date[0][3:], ''.join(content).strip()])
-                    csv_file.close()
+            csv_file.close()
 
 
 def main():
@@ -101,7 +140,7 @@ def main():
     #     page_count = getNumPage(code)
     #     print(page_count)
     #     # # 爬取内容
-    for i in range(1, 27):
+    for i in range(1, 31):
         reports('浦发银行', '600000', i)
         time.sleep(5)
 
