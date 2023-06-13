f5f7f0d32dda554a436c337ea42c78eb60bdb527
sadxdh <2461366932@qq.com>
2023-06-14 00:16:00 +0800

    update analysis data

--- analytics/news_analysis/report_analysis.py
+++ analytics/news_analysis/report_analysis.py
@@ -1,3 +1,39 @@
+# analytics/news_analysis/report_analysis.py
+#
+# report_analysis.py文件为股票研究报告进行数据处理的主体文件（部分处理在数据采集、可视化与模型构建部分）
+# xxx研究报告第x页_processed.csv的阶段性处理文件
+# 由于数据量较大，对于历史较长的股票，其研究报告大多数据量较大，单进程处理速度过慢
+# 此外，在生成thinWords的过程中，创建thulac.thulac对象需要导入语言处理模型，需要消耗大量的时间，所以特将模型接收变量设置为全局变量，可以大大减少模型读取的速度，使得脚本运行速度整体提高了40%左右
+# 此脚本使用了 python中的多进程技术，极大提高了数据出来的速度（提升的效果瓶颈为cpu的处理瓶颈与内存的容量），缺点是cpu、内存需求量过大，会一定程度上影响计算机其他任务的处理，导致出现卡顿等现象
+# 改进建议：可以调整使用线程池技术，线程创建进程数，保证计算机其他任务的正常运行
+#
+# 本脚本还配置了研究报告合并方法（concatCSV），此脚本内部可用于合并多进程任务后的所有中间文件的合并任务，生成xxx_report_total.csv'
+# 注：该脚本读取和写入的文件夹都为：data/news_data
+#
+# 主要作用：股票研究报告通用数据处理
+# 去除了 标题	报告类型	发布日期	机构	研究员 五列数据，只保留了日期和报告内容
+# content 每一条都是该天所有报告的汇总，对源数据进行了修改
+# 通过研究报告内容生成了 coarseWords、thinWords、keyWords、affectiveClassification
+#
+#   coarseWords(粗分词)（未统计词频）
+#     使用nltk库 （from nltk.corpus import stopwords）
+#      去除 标点符号 转换为小写（英文） 分词 去除停用词（chanese）
+#       从每一份研究报告中提取 相对重要词
+# thinWords（细分词）（词频统计）
+#   使用thulac库(import thulac)
+#     根据中国人的语言习惯对每一份研究报告进行处理
+#       去除 标点符号等无效字符 词性标注 词频统计
+#           统计结果保存为 List(tuple)类型
+#              备注：此处分组聚合需要提供as_index=False参数，含义表示 分组不设为索引，避免无法直接将分组聚合结果直接转换为二元组列表
+# keyWords（关键词）：
+#    使用jieba库（import jieba.analyse）
+#      对研究报告内容进行关键字分析，并提取关键词权重
+#        统计结果保存为 List(tuple)类型
+# affectiveClassification（情感分类）：
+#    使用nltk库（from nltk.sentiment import SentimentIntensityAnalyzer）
+#      分为三类，包括：积极、中性、消极
+#       对研究报告进行情感分析（发现部分 积极和消极的研究报告）
+
 import pandas as pd
 import os
 import re
@@ -7,15 +43,19 @@ import jieba
 import thulac
 from concurrent.futures import ProcessPoolExecutor
 
-stockname = '上海临港'
+stockname = '海康威视'
 path = 'data/news_data/'  # 右侧要有‘/’
+thu1 = thulac.thulac(filt=True)  # 词性标注
+
 
 def readCSV(file):
     report_df = pd.read_csv(os.path.join(path, file), names=['title', 'institution_type', 'institution_name', 'author', 'date', 'content'])
+    report_df = report_df[['date', 'content']]
     report_df.set_index(report_df['date'], inplace=True)
     report_df.sort_index(inplace=True)  # 排序后生效，改变原数据
     report_df.drop(report_df['date'])
     return report_df
+
 
 def coarseWords(report_df):
     def func(x):
@@ -36,25 +76,27 @@ def coarseWords(report_df):
     report_df['coarseWords'] = report_df['content'].apply(func)
     return report_df
 
+
 def thinWords(report_df):
     def func(x):
-        thu1 = thulac.thulac(filt=True)  # 词性标注
+        x = re.sub(r'[^\u4e00-\u9fa5]+', '', x)
         df = pd.DataFrame(thu1.cut(x), columns=['word', 'counts'])
-        word_count = df[df['counts'] == 'v'].groupby('word').count()
-        word_count_list = word_count.to_records().tolist()
-        # print(word_count_list)
-        return word_count_list
-    report_df['thinWords'] = report_df['coarseWords'].apply(func)
+        word_count = df[df['counts'] == 'v'].groupby('word', as_index=False).count()
+        word_count_tuples = list(word_count.to_records(index=False))
+        return word_count_tuples
+    report_df['thinWords'] = report_df['content'].apply(func)
     return report_df
+
 
 def keyWords(report_df):
     def func(x):
         import jieba.analyse
         keywords = jieba.analyse.extract_tags(x, topK=10, withWeight=True)
-        print(keywords)
+        # print(keywords)
         return keywords
     report_df['keyWords'] = report_df['content'].apply(func)
     return report_df
+
 
 def affectiveClassification(report_df):
     def func(x):
@@ -79,11 +121,13 @@ def affectiveClassification(report_df):
     report_df['affectiveClassification'] = report_df['content'].apply(func)
     return report_df
 
+
 def multi_process():
     files = [file for file in os.listdir(path) if re.match(rf'{stockname}研究报告.*\.csv', file)]
     print(files)
     with ProcessPoolExecutor() as pool:
         pool.map(report_auto_process, files)
+
 
 # cpu密集型，使用多进程加速
 def report_auto_process(file):
@@ -92,15 +136,17 @@ def report_auto_process(file):
     report_df = thinWords(report_df)
     report_df = keyWords(report_df)
     report_df = affectiveClassification(report_df)
-    print(report_df.columns)
+    # print(report_df.columns)
     # file = ‘海康威视研究报告第1页.csv’ 保存文件名为：海康威视研究报告第1页_processed.csv
     savefilename = path + file.split('.')[0]+'_processed.'+file.split('.')[1]
     print(savefilename)
     report_df.to_csv(savefilename, encoding="utf_8_sig")
 
+
 def save_total(report_df):
     report_df.to_csv(f'{path}/{stockname}_report_total.csv', encoding="utf_8_sig")
     print(report_df)
+
 
 def concatCSV():
     report_df = pd.DataFrame()
@@ -108,12 +154,17 @@ def concatCSV():
     for file in files:
         if re.match(rf'{stockname}研究报告.*_processed\.csv', file):
             report_df = pd.concat([report_df, pd.read_csv(os.path.join(path, file))], axis=0)
+    report_df['content'] = report_df.groupby('date')['content'].transform(lambda x: ' '.join(x))
+    report_df = report_df.drop_duplicates(subset='content')
+    print("content列处理后表shape：", report_df.shape)
     save_total(report_df)
     return report_df
+
 
 def main():
     multi_process()
     concatCSV()
+
 
 if __name__ == '__main__':
     main()
--- data_collection/news_collection/getStockResearchReport.py
+++ data_collection/news_collection/getStockResearchReport.py
@@ -1,5 +1,56 @@
-# https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=002415&t1=all&p=1
-# 海康威视 近半年研究报告
+# 数据采集-研究报告-爬虫部分
+
+# 数据采集  新浪财经  股票对应研究报告
+# data_collection/news_collection/getStockResearchReport.py
+# 示例网址：https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=002415&t1=all&p=1
+# url两个重要参数：
+#   symbol：股票代码
+#   p：研究报告页码
+# 网站反爬机制
+# 同一ip频繁访问会有五分钟的拒绝访问，具体表现为：页面重定向
+#   添加headers应对反扒效果并不明显
+#   解决方案：使用代理池（在开源项目基础上进行改写）
+#     源项目：https://github.com/Python3WebSpider/ProxyPool.git
+#     代理池原理：三大部分：获取模块、存储模块、检查模块
+#        获取模块：https://cuiqingcai.com/7048.html
+#        存储模块：https://cuiqingcai.com/7048.html
+#        检查模块：https://cuiqingcai.com/7048.html
+
+# 使用requests，Redis数据库，etree，xpath，CSV
+# 设置全局变量为headers，使用fake_useragent伪造UA
+
+# 爬虫脚本
+# getStockResearchReport.py
+# getproxy:
+#   连接到本地Redis数据库
+#      redis中使用key获取value，从所有的value中提取得分最高的代理
+#   获取所有键名为"use_proxy"的键的值
+#   从值中筛选出"last_status": true的键
+#   从筛选后的键中随机选择一个键
+
+# getNumPage(code):
+# https://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=002415&t1=all
+# 使用requests访问研究报告首页，获取研究报告页数长度
+
+# reports:
+# 单独页面的研究报告爬虫，用于爬取页面内所有研究报告对应页面的报告详情，然后保存到CSV文件中
+# 每一个单独页面的所有研究报告保存在一个CSV表格中
+# 在循环请求与分析单独研究报告之前创建CSV对象，请求的同时对CSV文件进行写入，在每一页全部爬取完成时释放变量
+# title（标题）, category（机构类型）, institution（公司名称）, researcher（研究员）, date（日期）, content（内容）
+# 为了提高代码的健壮性，其中添加了异常捕获结构，用于判断页面内容与字段内容是否正常获取
+
+# main（主程序）:
+# 读取target文件中的目标股票名称和股票对应代码
+# 使用循环调用方法实现自动爬取
+# 设置时间间隔，一定程度上减少网页访问拒绝
+
+#
+# 改进：可以添加多线程模块，加快爬取速度
+# 指定研究报告下载.py
+# 可实现自动爬取，生成xxxx研究报告第x页.csv'
+# 注：该脚本读取和写入的文件夹都为：data/news_data
+# 在getStockResearchReport.py的基础上改写，实现对某一特定研究报告的爬取，防止循环爬取脚本部分信息获取不全问题
+
 import requests
 from lxml import etree
 import csv
@@ -105,3 +156,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
